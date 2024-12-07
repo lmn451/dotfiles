c3d9d692df050b6d7b0b81cffb1085af2ed71158
lmn451 <putaluta@tuta.io>
2024-12-07 13:17:47 +0200

    fix meta to esc+

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.keymap.set("n", "<M-j>", ":m .+1<CR>==") -- Alt-j to move line down
+vim.keymap.set("n", "<M-k>", ":m .-2<CR>==") -- Alt-k to move line up
+vim.keymap.set("i", "<M-j>", "<Esc>:m .+1<CR>==gi")
+vim.keymap.set("i", "<M-k>", "<Esc>:m .-2<CR>==gi")
